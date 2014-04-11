5fec77d4ad3de6db74c37e80a11cc0dbe2598ac0
Rob Mills <robjmills@gmail.com>
2014-04-11 15:35:17 +0100

    switched to sync version of opening private.json

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var express = require('express')
   , Twit = require('twit')
   , io = require('socket.io').listen(server);
 
+// read credentials from private.json
 var fs = require('fs');
+var data = fs.readFileSync('./private.json');
 
 // listen on a l33t port
 server.listen(1337);
@@ -15,34 +17,24 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-var credentials;
+// parse creds from file
+var credentials = JSON.parse(data);
 
-// read credentials from private.json
-fs.readFile('./private.json', function (err, data) {
-  if (err) throw err;
+// init Twit
+var T = new Twit({
+  consumer_key: credentials.consumer_key,
+  consumer_secret: credentials.consumer_secret,
+  access_token: credentials.access_token,
+  access_token_secret: credentials.access_token_secret
+})
 
-  // parse creds from file
-  credentials = JSON.parse(data);
+// array of terms to track
+var trackList = ['untp it'];
 
-  // init Twit
-  var T = new Twit({
-    consumer_key: credentials.consumer_key,
-    consumer_secret: credentials.consumer_secret,
-    access_token: credentials.access_token,
-    access_token_secret: credentials.access_token_secret
-  })
-
-  // array of terms to track
-  var trackList = ['untp it'];
-
-  // open socket connection
-  io.sockets.on('connection', function (socket){
-    var stream = T.stream('statuses/filter', { track: trackList })
-    stream.on('tweet', function (tweet) {
-      io.sockets.emit('stream',tweet);
-    });
+// open socket connection
+io.sockets.on('connection', function (socket){
+  var stream = T.stream('statuses/filter', { track: trackList })
+  stream.on('tweet', function (tweet) {
+    io.sockets.emit('stream',tweet);
   });
-
 });
-
-
